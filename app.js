3d5c08d3abf08d860bf52df038875dd9c305dc79
shinpei <shinpei.nakata@gmail.com>
2014-04-27 14:32:05 +0900

    make port change

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {"Content-type": "text/html"});
 	var output = fs.readFileSync("html/index.html", "utf-8");
 	res.end(output);
-	}).listen(process.env.VMC_APP_PORT || 3000);
-
+})
 var io = socketio.listen(server);
 
+
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
+var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+server.listen(port, function() {
+  console.log("http server listening on port %d ", server.address().port);
+});
