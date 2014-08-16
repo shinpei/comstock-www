68995047a3d1b689581c0632696ddc6b50d46dc8
shinpei <shinpei.nakata@gmail.com>
2014-08-16 16:10:54 +0900

    loginas update Errors

--- loginas.go
+++ loginas.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/shinpei/comstock-www/model"
 	cmodel "github.com/shinpei/comstock/model"
 	"labix.org/v2/mgo"
@@ -15,29 +14,31 @@ const (
 	SESSION_COLLECTON string = "session"
 )
 
-func CheckSession(db *mgo.Database, authinfo string) (err error) {
+func CheckSession(db *mgo.Database, token string) (err error) {
 	c := db.C(SESSION_COLLECTON)
-	q := c.Find(bson.M{"token": authinfo})
+	q := c.Find(bson.M{"token": token})
 	count, _ := q.Count()
 	if count == 1 {
 		// found session
 
 	} else {
 		// not found session. login requires
-		fmt.Println("Couldn't found session")
+		log.Println("Couldn't found session")
 		err = cmodel.ErrSessionNotFound
 	}
 	return
 }
 
 func LoginAs(db *mgo.Database, l *model.LoginRequest) (s *model.Session, err error) {
-	c := db.C(AUTH_COLLECTION)
-	iter := c.Find(nil).Iter()
-	var item cmodel.AuthInfo
-	count := 0
-	for iter.Next(&item) {
-		log.Println(item.Mail())
-		count++
+	c := db.C(USER_COLLECTION)
+	q := c.Find(bson.M{"mail": l.Mail()})
+	count, _ := q.Count()
+	if count == 1 {
+		// found user
+
+	} else {
+		log.Println("Counln't find user")
+		err = cmodel.ErrUserNotFound
 	}
 	return
 }
