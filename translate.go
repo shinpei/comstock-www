dc811deaf76766d3fcfdf2d181f8aa0e7a53a8fe
shinpei <shinpei.nakata@gmail.com>
2014-09-20 11:52:13 +0900

    add translate

---
+++ translate.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/shinpei/comstock-www/model"
+	cmodel "github.com/shinpei/comstock/model"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"log"
+)
+
+func TranslateHandler(w http.ResponseWriter, req *http.Request) {
+	session, db := getSessionAndDB()
+	defer session.Close()
+	param, _ := url.ParseQuery(req.URL.RawQuery)
+	if param["authinfo"] == nil || aram["authinfo"] == nil {
+		http.Error(w, "Invalid post command request", http.StatusBadRequest)
+	}
+	err := translateCommand(db, param["authinfo"][0], param["cmd"][0])
+	if _, ok := err.(*cmodelSessionExpiresError); ok {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	} else if _, ok := err.(*cmodel.SessionNotFoundError); ok {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+	w.Write([]byte("Success"))
+}
+
+func translateCommand(db *mgo.Database, token string, cmd string) (err error) {
+
+}
