92359c4ab782eefd4bc130081f12c825095eb540
shinpei <shinpei.nakata@gmail.com>
2014-10-29 00:42:26 +0900

    update models

--- model/translate_test.go
+++ model/translate_test.go
@@ -7,6 +7,19 @@ import (
 	"testing"
 )
 
+func TestCreateNewCommandItem(t *testing.T) {
+	_, ci := CreateNewCommandItem(1, "ls -la")
+	AssertEqual(t, "ls -la", ci.Command)
+}
+
+func TestCreateFlow(t *testing.T) {
+	_, ci1 := CreateNewCommandItem(1, "ls -la")
+	_, ci2 := CreateNewCommandItem(1, "wc -l")
+	_, f := CreateFlow([]*NewCommandItem{ci1, ci2})
+
+	fmt.Println(f.Command())
+}
+
 func TestTrans(t *testing.T) {
 	c1 := CreateCommandItem(1, "ls -la")
 	history := TranslateCommand1to2(c1)
@@ -19,4 +32,5 @@ func TestHistoryCommand(t *testing.T) {
 	c1 := CreateCommandItem(1, "ls -la")
 	history := TranslateCommand1to2(c1)
 	fmt.Printf("cmd:%s\n", history.Command())
+
 }
